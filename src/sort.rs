e65fd7009baf31566e75d56f5ba75884b592f531
Anton Marsden <anton.marsden@ninetyten.co.nz>
2020-05-10 23:29:01 +1200

    Moved re-ordering logic into a separate function.

--- src/sort.rs
+++ src/sort.rs
@@ -9,12 +9,35 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 
+/// Common re-ordering logic used internally by the SortOps trait.
+fn re_order<F>(ordering : &mut [usize], mut swap_func : F)
+where F: FnMut(usize, usize)
+{
+    // swap rows/columns until everything is in the right spot
+    for i in 0..ordering.len() {
+        if i != ordering[i] {
+            let mut j = i;
+            loop {
+                let k = ordering[j];
+                if i == k {
+                    break;
+                }
+                swap_func(j, k);
+                ordering[j] = j;
+                j = k;
+            }
+            ordering[j] = j;
+        }
+    }
+}
+
 /// This trait provides sorting capabilities. Sorting of the rows and columns is performed in-place,
-/// and care is taken to minimise row/col swaps.
+/// and care is taken to reduce row/col swaps. This is achieved by sorting with the original
+/// indices, then repositioning the rows/columns once the new sort order has been determined.
 pub trait SortOps<T> : TooDeeOpsMut<T> {
     
     /// Sort the entire two-dimensional array by comparing elements on a specific row.
-    fn sort_by_row<F>(&mut self, row: usize, ref mut compare: F)
+    fn sort_by_row<F>(&mut self, row: usize, mut compare: F)
         where
         F: FnMut(&T, &T) -> Ordering, 
     {
@@ -22,28 +45,13 @@ pub trait SortOps<T> : TooDeeOpsMut<T> {
         let mut sort_data : Vec<(usize, &T)> = self[row].iter().enumerate().collect();
         sort_data.sort_by(|(_, vi), (_, vj)| compare(vi, vj));
         
-        let mut index : Vec<usize> = sort_data.iter().map(|(i, _)| *i).collect();
-
-        // Now swap columns until everything is in the right spot
-        for i in 0..index.len() {
-            if i != index[i] {
-                let mut j = i;
-                loop {
-                    let k = index[j];
-                    if i == k {
-                        break;
-                    }
-                    self.swap_cols(j, k);
-                    index[j] = j;
-                    j = k;
-                }
-                index[j] = j;
-            }
-        }
+        let mut ordering : Vec<usize> = sort_data.iter().map(|(i, _)| *i).collect();
+        
+        re_order(&mut ordering, |a, b| self.swap_cols(a, b));
     }
 
     /// Sort the entire two-dimensional array by comparing elements on in a specific column.
-    fn sort_by_col<F>(&mut self, col: usize, ref mut compare: F)
+    fn sort_by_col<F>(&mut self, col: usize, mut compare: F)
         where
         F: FnMut(&T, &T) -> Ordering, 
     {
@@ -51,24 +59,9 @@ pub trait SortOps<T> : TooDeeOpsMut<T> {
         let mut sort_data : Vec<(usize, &T)> = self.col(col).enumerate().collect();
         sort_data.sort_by(|(_, vi), (_, vj)| compare(vi, vj));
         
-        let mut index : Vec<usize> = sort_data.iter().map(|(i, _)| *i).collect();
+        let mut ordering : Vec<usize> = sort_data.iter().map(|(i, _)| *i).collect();
         
-        // Now swap rows until everything is in the right spot
-        for i in 0..index.len() {
-            if i != index[i] {
-                let mut j = i;
-                loop {
-                    let k = index[j];
-                    if i == k {
-                        break;
-                    }
-                    self.swap_rows(j, k);
-                    index[j] = j;
-                    j = k;
-                }
-                index[j] = j;
-            }
-        }
+        re_order(&mut ordering, |a, b| self.swap_rows(a, b));
     }
 }
 
